f53a4ac7029b38015c529d53b4060b12b6ee11ff
HayMiqo <mikael.gg@mail.ru>
2025-04-13 22:52:53 +0300

    release

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import './style.scss'
+
+const passwordField = document.querySelector('.password__field')
+const generateBtn = document.querySelector('.password__button')
+
+function passwordGeneration() {
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numbers = '0123456789'
+    const symbols = '!@#$%^&*()_+{}[]'
+
+    const allChars = lowercase + uppercase + numbers + symbols
+    const passwordLength = 12
+    let password = ''
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length)
+        password += allChars[randomIndex]
+    };
+
+    return password
+};
+
+generateBtn.addEventListener('click', () => {
+    const password = passwordGeneration()
+    passwordField.value = password
+})
